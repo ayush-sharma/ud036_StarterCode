182557ec30d3f16a0ba933036f726b66c45dd199
Ayush Sharma <ayushsharma.22+github@gmail.com>
2018-10-26 05:11:07 +0530

    File now complies with PEP8 line endings.

--- start_entertainment_center.py
+++ start_entertainment_center.py
@@ -7,43 +7,59 @@ import fresh_tomatoes
 
 movie_1 = movie.MyMovie(
     "A Star Is Born",
-    "A musician helps a young singer find fame, even as age and alcoholism send his own career into a downward spiral.",
-    "https://m.media-amazon.com/images/M/MV5BMjE3MDQ0MTA3M15BMl5BanBnXkFtZTgwMDMwNDY2NTM@._V1_UX182_CR0,0,182,268_AL__QL50.jpg",
+    "A musician helps a young singer find fame, even as age and alcoholism"
+    + " send his own career into a downward spiral.",
+    "https://m.media-amazon.com/images/M/MV5BMjE3MDQ0MTA3M15BMl5BanBnXkFtZT"
+    + "gwMDMwNDY2NTM@._V1_UX182_CR0,0,182,268_AL__QL50.jpg",
     "https://www.youtube.com/watch?v=nSbzyEJ8X9E"
     )
 
 movie_2 = movie.MyMovie(
     "Venom",
-    "When Eddie Brock acquires the powers of a symbiote, he will have to release his alter-ego \"Venom\" to save his life.",
-    "https://m.media-amazon.com/images/M/MV5BNzAwNzUzNjY4MV5BMl5BanBnXkFtZTgwMTQ5MzM0NjM@._V1_UX182_CR0,0,182,268_AL__QL50.jpg",
+    "When Eddie Brock acquires the powers of a symbiote, he will have to rel"
+    + "ease his alter-ego \"Venom\" to save his life.",
+    "https://m.media-amazon.com/images/M/MV5BNzAwNzUzNjY4MV5BMl5BanBnXkFtZT"
+    + "gwMTQ5MzM0NjM@._V1_UX182_CR0,0,182,268_AL__QL50.jpg",
     "https://www.youtube.com/watch?v=u9Mv98Gr5pY"
     )
 
 movie_3 = movie.MyMovie(
     "Goosebumps 2: Haunted Halloween",
-    " Two young friends find a magic book that brings a ventriloquist's dummy to life.",
-    "https://m.media-amazon.com/images/M/MV5BNzgxMDQ2MDUyMF5BMl5BanBnXkFtZTgwNzgyMjQyNjM@._V1_UX182_CR0,0,182,268_AL__QL50.jpg",
+    "Two young friends find a magic book that brings a ventriloquist's dummy"
+    + " to life.",
+    "https://m.media-amazon.com/images/M/MV5BNzgxMDQ2MDUyMF5BMl5BanBnXkFtZTg"
+    + "wNzgyMjQyNjM@._V1_UX182_CR0,0,182,268_AL__QL50.jpg",
     "https://www.youtube.com/watch?v=nQeOzfm-lps"
     )
 
 movie_4 = movie.MyMovie(
     "First Man",
-    "A look at the life of the astronaut, Neil Armstrong, and the legendary space mission that led him to become the first man to walk on the Moon on July 20, 1969.",
-    "https://m.media-amazon.com/images/M/MV5BMDBhOTMxN2UtYjllYS00NWNiLWE1MzAtZjg3NmExODliMDQ0XkEyXkFqcGdeQXVyMjMxOTE0ODA@._V1_UX182_CR0,0,182,268_AL__QL50.jpg",
+    "A look at the life of the astronaut, Neil Armstrong, and the legendary"
+    + " space mission that led him to become the first man to walk on the"
+    + " Moon on July 20, 1969.",
+    "https://m.media-amazon.com/images/M/MV5BMDBhOTMxN2UtYjllYS00NWNiLWE1M"
+    + "zAtZjg3NmExODliMDQ0XkEyXkFqcGdeQXVyMjMxOTE0ODA@._V1_UX182_CR0,0,182"
+    + ",268_AL__QL50.jpg",
     "https://www.youtube.com/watch?v=PSoRx87OO6k"
     )
 
 movie_5 = movie.MyMovie(
     "Smallfoot",
-    "A Yeti is convinced that the elusive creatures known as \"humans\" really do exist. ",
-    "https://m.media-amazon.com/images/M/MV5BMTc5NzI1NjY4MV5BMl5BanBnXkFtZTgwNDIxNTIyNDM@._V1_UX182_CR0,0,182,268_AL__QL50.jpg",
+    "A Yeti is convinced that the elusive creatures known as \"humans\""
+    + " really do exist. ",
+    "https://m.media-amazon.com/images/M/MV5BMTc5NzI1NjY4MV5BMl5BanBnXkFt"
+    + "ZTgwNDIxNTIyNDM@._V1_UX182_CR0,0,182,268_AL__QL50.jpg",
     "https://www.youtube.com/watch?v=34cHO5_LX9g"
     )
 
 movie_6 = movie.MyMovie(
     "The Old Man & the Gun",
-    "Based on the true story of Forrest Tucker and his audacious escape from San Quentin at the age of 70 to an unprecedented string of heists that confounded authorities and enchanted the public. ",
-    "https://m.media-amazon.com/images/M/MV5BOTk3NjU5MjIxM15BMl5BanBnXkFtZTgwNjU0OTU2NTM@._V1_UX182_CR0,0,182,268_AL__QL50.jpg",
+    "Based on the true story of Forrest Tucker and his audacious"
+    + " escape from San Quentin at the age of 70 to an unprecedented"
+    + " string of heists that confounded authorities and enchanted "
+    + "the public. ",
+    "https://m.media-amazon.com/images/M/MV5BOTk3NjU5MjIxM15BMl5Ba"
+    + "nBnXkFtZTgwNjU0OTU2NTM@._V1_UX182_CR0,0,182,268_AL__QL50.jpg",
     "https://www.youtube.com/watch?v=d7rlUe-Thvk"
     )
 
